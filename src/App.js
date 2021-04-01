2f0b760c6d0d7a3c7950c43492e0b3030e8117c9
JorPoon <jormpoon@gmail.com>
2021-04-01 18:40:49 -0400

    changing App component from functional to class with google Auth

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import Homepage from "./pages/homepage/Homepage.component.jsx"
 import ShopPage from './pages/shop/ShopPage.component.jsx';
 import Header from "./components/header/Header.component.jsx"
 import SignInAndSignUpPage from "./pages/signIn-signUp/SignIn-SignUp.component.jsx"
+import { auth } from "./firebase/firebase.utils"
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Homepage}/>
-        <Route path="/shop" component={ShopPage}/>
-        <Route path="/signin" component={SignInAndSignUpPage}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+          <Route path="/shop" component={ShopPage}/>
+          <Route path="/signin" component={SignInAndSignUpPage}/>
+        </Switch>
+      </div>
+    );
+  } 
 }
 
 export default App;
