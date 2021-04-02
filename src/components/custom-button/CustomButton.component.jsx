be72b556fd46e7f4726b4d728b89b1e2a593403a
JorPoon <jormpoon@gmail.com>
2021-04-01 22:38:59 -0400

    fixing typo on custom button classname

--- src/components/custom-button/CustomButton.component.jsx
+++ src/components/custom-button/CustomButton.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./CustomButton.styles.scss"
 
 const CustomButton = ({children, logInMethod, isGoogleSignIn, ...otherButtonProps}) => (
-    <button onClick={logInMethod} className={`${ isGoogleSignIn ? "google-sign-in" : "" }custom-button`} {...otherButtonProps}>
+    <button onClick={logInMethod} className={`${ isGoogleSignIn ? "google-sign-in" : "" } custom-button`} {...otherButtonProps}>
         {children}
     </button>
 )
