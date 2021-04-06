60da87b7d7f62585faeaae28c48d00eb192c06d6
JorPoon <jormpoon@gmail.com>
2021-04-05 22:55:12 -0400

    completed sign in and sign up functionality with firebase auth

--- src/pages/signIn-signUp/SignIn-SignUp.component.jsx
+++ src/pages/signIn-signUp/SignIn-SignUp.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/Sign-in.component.jsx"
-
+import SignUp from "../../components/sign-up/Sign-Up.component.jsx"
 import "./SignIn-SignUp.styles.scss"
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 )
 
