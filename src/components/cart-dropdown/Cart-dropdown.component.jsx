bfb53f4d91c960924fb3acc4dde3ee2e9fa3e43b
JorPoon <jormpoon@gmail.com>
2021-04-13 23:17:37 -0400

    cart drop down will now show the cart items' details

--- src/components/cart-dropdown/Cart-dropdown.component.jsx
+++ src/components/cart-dropdown/Cart-dropdown.component.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/CustomButton.component.jsx";
+import CartItem from "../cart-item/Cart-item.component.jsx";
 import "./Cart-dropdown.styles.scss";
 
-const CartDropDrown = () => (
+const CartDropDrown = ({cartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-items" />
+        <div className="cart-items">
+            {
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                ))
+            }
+            </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropDrown
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems,
+})
+
+export default connect(mapStateToProps)(CartDropDrown)
