ec4d13713e0e539b63973ebd493c39c0d287fc0b
JorPoon <jormpoon@gmail.com>
2021-04-14 22:56:56 -0400

    implementing selectors for files with mapstatetoprops

--- src/components/cart-dropdown/Cart-dropdown.component.jsx
+++ src/components/cart-dropdown/Cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { selectCartItems } from "../../redux/cart/cart.selectors"
+import { createStructuredSelector } from "reselect";
 import CustomButton from "../custom-button/CustomButton.component.jsx";
 import CartItem from "../cart-item/Cart-item.component.jsx";
 import "./Cart-dropdown.styles.scss";
@@ -18,8 +19,8 @@ const CartDropDrown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 })
 
 export default connect(mapStateToProps)(CartDropDrown)
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,6 +4,11 @@ import { createSelector } from "reselect";
 
 const selectCart = state => state.cart;
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    (cart) => cart.hidden
+)
+
 export const selectCartItems = createSelector(
     [selectCart],
     (cart => cart.cartItems)
