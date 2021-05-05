b4f8de5fb9692c4f2de1bb3bb95b9979d930f28d
JorPoon <jormpoon@gmail.com>
2021-05-04 21:23:35 -0400

    implementing collectionUrlParam for dynamic routing and installing lodash memoize

--- src/pages/collection/Collection.component.jsx
+++ src/pages/collection/Collection.component.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection, selectCollections } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/Collection-item.component.jsx";
 
 import "./Collection.styles.scss";
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
     <div className="collection-page">
         <h2>Collection Page</h2>
     </div>
 )
 
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollections(ownProps.match.params.collectionID)(state)
+})
 
-export default CollectionPage
+export default connect(mapStateToProps)(CollectionPage);
