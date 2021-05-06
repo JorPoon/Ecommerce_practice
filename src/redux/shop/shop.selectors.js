66301872ee6957e0e2d039b4f9fc2a3d9e41fd5e
JorPoon <jormpoon@gmail.com>
2021-05-05 21:15:51 -0400

    qcurrying data normalization through collections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,6 @@
 import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectShop = state => state.shop
 
@@ -17,7 +10,9 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
-export const selectCollection = memoize((collectionUrlParam) => createSelector(
-    [selectCollections],
-    collections => collections.find( collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-))
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    )
+);
