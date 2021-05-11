9f7523349f34a1f2596aae41a763f2e3677ec9b0
JorPoon <jormpoon@gmail.com>
2021-05-10 22:52:18 -0400

    setting up stripe checkout component

--- src/components/stripe-button/Stripe-button.component.jsx
+++ src/components/stripe-button/Stripe-button.component.jsx
@@ -4,4 +4,8 @@ import StripeCheckout from "react-stripe-checkout";
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "";
+
+    return (
+        <StripeCheckout/>
+    )
 }
