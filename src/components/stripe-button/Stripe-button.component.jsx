4b15f8c84ecb74e84604950099b95b04b370e24f
JorPoon <jormpoon@gmail.com>
2021-05-12 22:15:51 -0400

    set up stripe checkout button component on to checkout page, need pusblishable key from stripe account to work

--- src/components/stripe-button/Stripe-button.component.jsx
+++ src/components/stripe-button/Stripe-button.component.jsx
@@ -5,7 +5,23 @@ const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "";
 
+    const onToken = token => {
+        alert("Payment Successful")
+    }
+
     return (
-        <StripeCheckout/>
+        <StripeCheckout
+            label="Pay Now"
+            name="JMP Clothing"
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Your total is $${price}`}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
     )
 }
+
+export default StripeCheckoutButton
